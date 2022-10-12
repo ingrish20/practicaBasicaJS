af2d0fb51678f38f8705981efc693aa4fa7addbe
ingrish20 <ingrish.perez@gmail.com>
2022-10-12 12:08:15 -0600

    firstPractice

---
+++ practica1.js
@@ -0,0 +1,7 @@
+var person1 = prompt("write your name");
+var person2 = prompt("write your crush name");
+var n = Math.random();
+n = n * 100;
+n = Math.floor(n)+1;
+console.log(person1 + ' and ' + person2 + ' are in love ' + n + '%');
+
